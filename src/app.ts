6bfc399a23bf78f48e310eb69ccc299be8b18998
MujuniEli <elijahmujuni@gmail.com>
2023-11-08 17:20:17 +0300

    Added form event listener

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,8 @@ const type = document.querySelector('#type') as HTMLSelectElement;
 const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault()
+
+})
