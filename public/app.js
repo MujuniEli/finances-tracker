cf7259b21ffc25b5aceff0b3f126d6a85628545a
MujuniEli <elijahmujuni@gmail.com>
2023-11-09 08:16:53 +0300

    Added interfaces

--- public/app.js
+++ public/app.js
@@ -1,6 +1,9 @@
-"use strict";
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const toFrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+export {};
